2d0be46afde8c6e91d438b7cd88ce67df36ec6b8
Liam OConnell <mloconnell16@gmail.com>
2026-02-06 10:49:08 -0800

    fix firebase imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { auth, db } from "./firebase";
+import { auth, db } from "./firebase.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
